766c16335ee7034c4652b0f4e6f98fe9d1973414
azaelthecrak <32691893+azaelthecrak@users.noreply.github.com>
2020-01-18 09:34:21 -0600

    Galeria y enviar a correo PHP

---
+++ correo.php
@@ -0,0 +1,12 @@
+<?php
+$correo = 'azael.goku@hotmail.com';
+
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$msj = $_POST['msj'];
+
+$asunto ="Mensaje enviado  desde Seguridad privada Aryoda";
+$msjCompleto = $msj . "\nAtentamente: ". $nombre;
+mail($correo, $msjCompleto, $asunto);
+echo "<script>alert ('correo enviado exitosamente')</script>";
+echo "<script>setTimeout(\"location.href='contacto.html'\",1000)</script>";
